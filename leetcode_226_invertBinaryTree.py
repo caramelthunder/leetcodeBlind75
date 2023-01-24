f6da01795946135fd53638b12924f18149aca326
thinh nguyen <Thinh Nguyen>
2023-01-24 11:27:50 -0500

    add unit testing class

--- leetcode_226_invertBinaryTree.py
+++ leetcode_226_invertBinaryTree.py
@@ -27,21 +27,38 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def numbers_to_tree(numbers: List[int]) -> TreeNode:
-    def helper(i: int) -> TreeNode:
-        if i >= len(numbers) or numbers[i] is None:
-            return None
-        node = TreeNode(numbers[i])
-        node.left = helper(i * 2 + 1)
-        node.right = helper(i * 2 + 2)
-        return node
-    return helper(0)
+class Helper:
+    def numbers_to_tree(self, numbers: List[int]) -> TreeNode:
+        def helper(i: int) -> TreeNode:
+            if i >= len(numbers) or numbers[i] is None:
+                return None
+            node = TreeNode(numbers[i])
+            node.left = helper(i * 2 + 1)
+            node.right = helper(i * 2 + 2)
+            return node
+        return helper(0)
 
-def tree_to_numbers(root: TreeNode) -> List[int]:
-    def helper(node: TreeNode) -> List[int]:
-        if not node:
-            return [None]
-        left_numbers = helper(node.left)
-        right_numbers = helper(node.right)
-        return [node.val] + left_numbers + right_numbers
-    return helper(root)
+    def tree_to_numbers(self, root: TreeNode) -> List[int]:
+        def helper(node: TreeNode) -> List[int]:
+            if not node:
+                return [None]
+            left_numbers = helper(node.left)
+            right_numbers = helper(node.right)
+            return [node.val] + left_numbers + right_numbers
+        return helper(root)
+
+class Solution1:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        pass
+
+import unittest
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.helper = Helper()
+        self.solution1 = Solution1()
+
+
+#################################
+if __name__ == '__main__':
+    unittest.main()
+    
