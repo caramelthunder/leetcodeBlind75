2bb4f99978552927bc4adc6ba2a9cf117156e36d
Thinh Nguyen <thinh.nguyen@everestre.com>
2023-02-01 11:25:58 -0500

    finished and tested

--- leetcode_543_diameterOfABinaryTree.py
+++ leetcode_543_diameterOfABinaryTree.py
@@ -27,3 +27,89 @@ The number of nodes in the tree is in the range [1, 104].
 -100 <= Node.val <= 100
 '''
 
+class TreeNode:
+  def __init__(self, val= None):
+    self.val = val
+    self.left = None
+    self.right = None
+
+class TreeHelper:
+  def arr_to_tree(self, arr: list[any]) -> TreeNode:
+
+    def _arr_to_tree(arr, pos= 0):
+      if not arr or pos >= len(arr) or arr[pos] is None:
+        return None
+
+      node = TreeNode(arr[pos])
+      node.left = _arr_to_tree(arr, pos * 2 + 1)
+      node.right = _arr_to_tree(arr, pos * 2 + 2)
+
+      return node
+    return _arr_to_tree(arr, 0)
+
+class Solution:
+  def diameterOfBinaryTree(self, root: TreeNode) -> int:
+    diameter = 0
+
+    def dfs(node):
+      nonlocal diameter
+
+      if not node:
+        return 0
+
+      left_height = dfs(node.left)
+      right_height = dfs(node.right)
+      local_diameter = left_height + right_height
+      diameter = max(diameter, local_diameter)
+
+      return max(left_height, right_height) + 1
+    
+    dfs(root)
+    return diameter
+
+import unittest
+class Test(unittest.TestCase):
+  def setUp(self):
+    self.treeHelper = TreeHelper()
+    self.solution = Solution()
+  
+  def test_example_1(self):
+    arr = [1,2,3,4,5]
+    expected_output = 3
+    actual_output = self.solution.diameterOfBinaryTree(root= self.treeHelper.arr_to_tree(arr= arr))
+    self.assertEqual(actual_output, expected_output)
+
+  def test_example_2(self):
+    arr = [1,2]
+    expected_output = 1
+    actual_output = self.solution.diameterOfBinaryTree(root= self.treeHelper.arr_to_tree(arr= arr))
+    self.assertEqual(actual_output, expected_output)
+
+  def test_example_3(self):
+    arr = [1,2,3,None,4,5,None]
+    expected_output = 4
+    actual_output = self.solution.diameterOfBinaryTree(root= self.treeHelper.arr_to_tree(arr= arr))
+    self.assertEqual(actual_output, expected_output)
+  
+  def test_empty_tree(self):
+    arr = []
+    expected_output = 0
+    actual_output = self.solution.diameterOfBinaryTree(root= self.treeHelper.arr_to_tree(arr= arr))
+    self.assertEqual(actual_output, expected_output)
+
+  def test_sided_tree(self):
+    arr = [1,2,None,3,None,None,None,4]
+    expected_output = 3
+    actual_output = self.solution.diameterOfBinaryTree(root= self.treeHelper.arr_to_tree(arr= arr))
+    self.assertEqual(actual_output, expected_output)
+
+  def test_dia_in_subtree(self):
+    arr = [1,None,3,None,None,4,5,None,None,None,None,None,None,None,6]
+    expected_output = 3
+    actual_output = self.solution.diameterOfBinaryTree(root= self.treeHelper.arr_to_tree(arr= arr))
+    self.assertEqual(actual_output, expected_output)
+
+####################################################
+if __name__ == '__main__':
+  unittest.main()
+
