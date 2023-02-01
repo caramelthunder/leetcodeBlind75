a94d106989d259823fd67fbd548f382482c589ff
Thinh Nguyen <thinh.nguyen@everestre.com>
2023-02-01 15:16:53 -0500

    finished and tested

--- leetcode_104_maximumDepthOfBinaryTree.py
+++ leetcode_104_maximumDepthOfBinaryTree.py
@@ -26,9 +26,51 @@ Constraints:
 The number of nodes in the tree is in the range [0, 104].
 -100 <= Node.val <= 100
 '''
+class TreeNode:
+  def __init__(self, val= None):
+    self.val = val
+    self.left = None
+    self.right = None
+
+class TreeHelper:
+  def arr_to_tree(self, arr: list[any], pos: int) -> TreeNode:
+    if not arr or pos >= len(arr) or arr[pos] is None:
+      return None
+
+    node = TreeNode(arr[pos])
+    node.left = self.arr_to_tree(arr, pos * 2 + 1)
+    node.right = self.arr_to_tree(arr, pos * 2 + 2)
+
+    return node
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        pass
+        if not root:
+          return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        
+        return max(left_depth, right_depth) + 1
 
+import unittest
+class Test(unittest.TestCase):
+  def setUp(self):
+    self.treeHelper = TreeHelper()
+    self.solution = Solution()
+  
+  def test_example_1(self):
+    arr = [3,9,20,None,None,15,7]
+    expected_output = 3
+    actual_output = self.solution.maxDepth(root= self.treeHelper.arr_to_tree(arr= arr, pos= 0))
+    self.assertEqual(actual_output, expected_output)
+
+  def test_example_2(self):
+    arr = [1,None,2]
+    expected_output = 2
+    actual_output = self.solution.maxDepth(root= self.treeHelper.arr_to_tree(arr= arr, pos= 0))
+    self.assertEqual(actual_output, expected_output)
+
+###########################################
+if __name__ == '__main__':
+  unittest.main()
     
