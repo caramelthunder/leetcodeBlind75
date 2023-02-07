04ae81e061c1210baf927973d56c06de9ae8e6d9
thinh nguyen <Thinh Nguyen>
2023-02-07 09:53:46 -0500

    setup folder

---
+++ leetcode_133_cloneGraph/unittest/leetcode_133_unittest.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import unittest
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from solutions import Solution
