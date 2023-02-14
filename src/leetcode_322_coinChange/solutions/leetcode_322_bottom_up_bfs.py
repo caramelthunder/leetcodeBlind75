317f0001f39adeed82254279c3ccd0928bd29aee
thinh nguyen <Thinh Nguyen>
2023-02-14 16:15:42 -0500

    remove unwanted text

--- src/leetcode_322_coinChange/solutions/leetcode_322_bottom_up_bfs.py
+++ src/leetcode_322_coinChange/solutions/leetcode_322_bottom_up_bfs.py
@@ -19,22 +19,3 @@ class Solution:
                     q.append((amount - coin, count + 1))
 
         return -1
-
-                
-
-
-
-'''
-Example 1:
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
-
-Example 2:
-Input: coins = [2], amount = 3
-Output: -1
-
-Example 3:
-Input: coins = [1], amount = 0
-Output: 0
-'''
--- src/leetcode_322_coinChange/solutions/leetcode_322_bottom_up_tabulation.py
+++ src/leetcode_322_coinChange/solutions/leetcode_322_bottom_up_tabulation.py
@@ -9,22 +9,3 @@ class Solution:
                 dp[amnt] = min(dp[amnt], dp[amnt - coin] + 1)
 
         return dp[amount] if dp[amount] != float('inf') else -1
- 
-                
-
-
-
-'''
-Example 1:
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
-
-Example 2:
-Input: coins = [2], amount = 3
-Output: -1
-
-Example 3:
-Input: coins = [1], amount = 0
-Output: 0
-'''
