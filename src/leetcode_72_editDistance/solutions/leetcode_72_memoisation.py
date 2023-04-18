91582d0622ad67a1c1a418fa8b6aa487754d33ef
thinh nguyen <Thinh Nguyen>
2023-04-17 21:33:10 -0400

    finished and tested leetcode 72

---
+++ src/leetcode_72_editDistance/solutions/leetcode_72_memoisation.py
@@ -0,0 +1,43 @@
+class Solution:
+    '''
+    Let M be the length of string word1 and N be the length of string word2.
+
+    Time Complexity: O(M * N)
+    As the memoization approach uses the cache, 
+    for every combination of word1 and word2 the result is computed only once.
+
+    Space Complexity: O(M * N)
+    The space is for the additional hashmap cache of size (M * N).
+    '''
+    def minDistance(self, word1: str, word2: str) -> int:
+        n1, n2 = len(word1), len(word2)
+        
+        # Call the helper function with initial indices and an empty cache dictionary
+        return self._min_distance(word1, word2, 0, 0, {})
+    
+    def _min_distance(self, word1, word2, i1, i2, cache):
+        # Base case: if either string is exhausted, return the remaining length of the other string
+        if i1 >= len(word1):
+            return len(word2) - i2
+        if i2 >= len(word2):
+            return len(word1) - i1
+        
+        # Check if the current indices are already in the cache
+        key = (i1, i2)
+        if key not in cache:
+            # If the characters at the current indices match, move on to the next indices
+            if word1[i1] == word2[i2]:
+                cache[key] = self._min_distance(word1, word2, i1 + 1, i2 + 1, cache)
+            else:
+                # Otherwise, recursively compute the minimum distance using each of the three operations
+                cache[key] = 1 + min(
+                    # Insert char into word1.
+                    self._min_distance(word1, word2, i1, i2 + 1, cache),
+                    # Delete char from word1.
+                    self._min_distance(word1, word2, i1 + 1, i2, cache),
+                    # Replace char from word1 with char from word2.
+                    self._min_distance(word1, word2, i1 + 1, i2 + 1, cache)
+                )
+        
+        # Return the result for the current indices
+        return cache[key]
---
+++ src/leetcode_72_editDistance/solutions/leetcode_72_tabulation.py
@@ -0,0 +1,44 @@
+class Solution:
+    '''
+    Let M be the length of string word1 and N be the length of string word2.
+
+    Time Complexity: O(M * N)
+    In the nested for-loop, the outer loop iterates M times, 
+    and the inner loop iterates N times.
+
+    Space Complexity: O(M * N)
+    The space is for the additional 2-dimensional array dp of size (M * N)).
+    '''
+
+    def minDistance(self, word1: str, word2: str) -> int:
+        n1, n2 = len(word1), len(word2)
+        
+        # create a 2D array for dynamic programming
+        dp = [[0] * (n1 + 1) for _ in range(n2 + 1)]
+        
+        # set base cases for when one of the words is empty
+        for r in range(1, len(dp)):
+            dp[r][0] = r
+        for l in range(1, len(dp[-1])):
+            dp[0][l] = l
+        
+        # fill the rest of the array with the minimum edit distance
+        for i2 in range(1, len(dp)):
+            for i1 in range(1, len(dp[0])):
+                char1, char2 = word1[i1 - 1], word2[i2 - 1]
+
+                # If the characters at the current indices match.
+                if char1 == char2:
+                    dp[i2][i1] = dp[i2 - 1][i1 - 1]
+                else:
+                    dp[i2][i1] = 1 + min(
+                        # Insert char into word1.
+                        dp[i2][i1 - 1],
+                        # Delete char from word1.
+                        dp[i2 - 1][i1],
+                        # Replace char from word1 with char from word2.
+                        dp[i2 - 1][i1 - 1]
+                    )
+        
+        # return the minimum edit distance
+        return dp[-1][-1]
