8efa2d1daf1da51db8092f1c922ed99a781e7ca5
Thinh Nguyen <thinh.nguyen.ce17@gmail.com>
2023-07-10 11:48:56 -0400

    update code leetcode_5

--- src/leetcode_5_longestPalindromicSubstring/solutions/leetcode_5_solution_plus.py
+++ src/leetcode_5_longestPalindromicSubstring/solutions/leetcode_5_solution_plus.py
@@ -1,34 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+
+        def _get_distance(start, end) -> int:
+            return end - start + 1
+
+        def _center_expansion(left, right) -> tuple[int, int]:
+            while 0 <= left <= right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return (left + 1, right - 1)
+
         max_palin_len = 0
         max_palin_range = (0, 0)
 
-        for i in range(n := len(s)):
+        i = 0
+        while i < (n := len(s)):
             j = i
             while j + 1 < n and s[j + 1] == s[i]:
                 j += 1  # expand to cover repeated chars
 
-            # even length
-            if j != i:
-                start, end = self._center_expansion(s, i, j)
-                if (palin_len := self._get_distance(start, end)) > max_palin_len:
-                    max_palin_len = palin_len
-                    max_palin_range = (start, end)
-                i = j  # skip over repeated char
-                
-            else:  # odd length
-                start, end = self._center_expansion(s, i, j)
-                if (palin_len := self._get_distance(start, end)) > max_palin_len:
-                    max_palin_len = palin_len
-                    max_palin_range = (start, end)
-           
-        return s[max_palin_range[0]: max_palin_range[1] + 1]
+            start, end = _center_expansion(i, j)
+            if (palin_len := _get_distance(start, end)) > max_palin_len:
+                max_palin_len = palin_len
+                max_palin_range = (start, end)
 
-    def _get_distance(self, start, end) -> int:
-        return end - start + 1
-    
-    def _center_expansion(self, s, left, right) -> tuple[int, int]:
-        while 0 <= left <= right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return (left + 1, right - 1)
+            i = j + 1
+
+        return s[max_palin_range[0]: max_palin_range[1] + 1]
