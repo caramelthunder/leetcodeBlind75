6c8bf47f9ad98fe4bf68d3a3520e0b068bd52796
ThinhNguyen <70381193+caramelthunder@users.noreply.github.com>
2023-08-10 10:26:40 -0400

    leetcode 1235 revised (#84)
    * leetcode 1235 revised
    
    * Update README file
    
    ---------
    
    Co-authored-by: Thinh Nguyen <thinh.nguyen.ce17@gmail.com>
    Co-authored-by: Thinh Nguyen <thinh.nguyence17@gmail.com>

--- src/leetcode_1235_maximumProfitIinJobScheduling/solutions/leetcode_1235_dp_binary_search_custom_object.py
+++ src/leetcode_1235_maximumProfitIinJobScheduling/solutions/leetcode_1235_dp_binary_search_custom_object.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Job:
-    start_time: int = 0
-    end_time: int = 0
-    profit: int = 0
+    start_time: int
+    end_time: int
+    profit: int
 
 
 class Solution:
@@ -28,33 +28,34 @@ class Solution:
             Time: O(n log n) due to sorting operation. The binary search in _job_search takes O(log n) time.
             Space: O(n) due to the 'jobs' list.
         """
+        # Error handling for inconsistent input lengths
+        if not (len(startTime) == len(endTime) == len(profit)):
+            raise ValueError("Input lists have inconsistent lengths.")
 
-        # Perform a binary search to find the last non-overlapping job before a given start time.
+        # Helper function to perform a binary search 
+        # to find the last non-overlapping job before a given start time
         def _job_search(target_end_time: int, left: int, right: int) -> int:
-            res = -1
+            non_overlapping_index = -1
             while left <= right:
                 mid = left + (right - left) // 2
                 job = jobs[mid]
                 if job.end_time <= target_end_time:
-                    res = mid
+                    non_overlapping_index = mid
                     left = mid + 1
                 else:
                     right = mid - 1
-            return res
+            return non_overlapping_index
 
         jobs = [Job(s, e, p) for s, e, p in zip(startTime, endTime, profit)]
-        jobs.sort(key=lambda job: job.end_time)  # Sort by job end_time
+        jobs.sort(key=lambda job: job.end_time)
+
+        max_profits = [job.profit for job in jobs]  # Separate list to store profits
 
         for i in range(1, len(jobs)):
             curr_job = jobs[i]
+            non_overlapping_index = _job_search(curr_job.start_time, 0, i)
+            
+            last_profit = max_profits[non_overlapping_index] if non_overlapping_index >= 0 else 0
+            max_profits[i] = max(max_profits[i - 1], last_profit + curr_job.profit)
 
-            last_non_overlap_job = Job()
-            if (res := _job_search(curr_job.start_time, 0, i)) >= 0:
-                last_non_overlap_job = jobs[res]
-
-            curr_job.profit = max(
-                jobs[i - 1].profit,
-                last_non_overlap_job.profit + curr_job.profit,
-            )
-
-        return jobs[-1].profit
+        return max_profits[-1] if max_profits else 0  # Handling the case of empty jobs
